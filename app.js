65e90c99d5152fe95f7d77d0ef5232c2828be39b
Ryan Grimard <ryan@everquote.com>
2023-07-16 16:35:34 -0400

    another

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
 const exercises = [
     'Burpee with double kick out',
     'Ab roll',
@@ -14,44 +11,53 @@ const exercises = [
     'military press',
     '30 second wall sit',
     'tricep dips'
-];
-
-app.get('/', (req, res) => {
-  // Generate the workout here
-  const workout = generateWorkout();
+  ];
   
-  // Send the workout as the response
-  res.send(workout);
-});
-
-function generateWorkout() {
-  let workout = '';
-
-  for (let i = 0; i < exercises.length; i++) {
-    const day = i + 1;
-    const exercise = exercises[i];
-    const ordinalSuffix = getOrdinalSuffix(day);
-
-    workout += `On the ${day}${ordinalSuffix} day of Christmas, my trainer gave to me: ${exercise}\n`;
+  function generateWorkout() {
+    const shuffledExercises = shuffleArray(exercises);
+  
+    const workoutList = document.getElementById('workoutList');
+    workoutList.innerHTML = '';
+  
+    for (let i = 0; i < shuffledExercises.length; i++) {
+      const day = i + 1;
+      const exercise = shuffledExercises[i];
+      const ordinalSuffix = getOrdinalSuffix(day);
+  
+      const listItem = document.createElement('li');
+      listItem.textContent = `On the ${day}${ordinalSuffix} day of Christmas, my trainer gave to me: ${exercise}`;
+  
+      workoutList.appendChild(listItem);
+    }
   }
-
-  return workout;
-}
-
-function getOrdinalSuffix(number) {
-  if (number % 10 === 1 && number !== 11) {
-    return 'st';
-  } else if (number % 10 === 2 && number !== 12) {
-    return 'nd';
-  } else if (number % 10 === 3 && number !== 13) {
-    return 'rd';
-  } else {
-    return 'th';
+  
+  function shuffleArray(array) {
+    const shuffled = [...array];
+  
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+  
+    return shuffled;
   }
-}
-
-const port = 3000; // Choose the desired port number
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  
+  function getOrdinalSuffix(number) {
+    if (number % 10 === 1 && number !== 11) {
+      return 'st';
+    } else if (number % 10 === 2 && number !== 12) {
+      return 'nd';
+    } else if (number % 10 === 3 && number !== 13) {
+      return 'rd';
+    } else {
+      return 'th';
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const regenerateButton = document.getElementById('regenerateButton');
+    regenerateButton.addEventListener('click', generateWorkout);
+  
+    generateWorkout();
+  });
+  
